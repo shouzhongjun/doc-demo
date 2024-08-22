fd07f860fc304c24cb1cba844dfca6fc79409fd5
ZeroBugHero <sdcx-zx@outlook.com>
2024-08-22 17:31:28 +0800

    golang 包

--- docs/.vitepress/relaConf/navbar.ts
+++ docs/.vitepress/relaConf/navbar.ts
@@ -4,18 +4,17 @@ import { DefaultTheme } from 'vitepress';
 export const nav: DefaultTheme.NavItem[] = [
     { text: '首页', link: '/' },
     {text:"个人作品展示",link:"works.md"},
-    {text:"Golang",link:"/golang"},
     { text: '关于作者', link: 'https://sugarat.top/aboutme.html' },
   {
-    text: '个人成长',
+    text: 'Golang',
     items: [
       {
-        text: '大江南北游记',
+        text: '基础',
         link: '/column/Travel/' // 表示docs/column/Travel/index.md
       },
       {
-        text: '所思·所想',
-        link: '/column/Growing/' // 表示docs/column/Growing/index.md
+        text: '包',
+        link: '/golang/包/' // 表示docs/column/Growing/index.md
       }
     ]
   },
---
+++ docs/golang/包/index.md
@@ -0,0 +1,6 @@
+---
+description: 默认支持流程图，tabs面板，待办列表，作品页面
+hidden: true
+---
+
+# 这里是Gloang语言的包
